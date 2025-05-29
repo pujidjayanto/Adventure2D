45d023213032cec736d487e5acdceb84cd51dfb6
pujidjayanto <pujidjayanto@gmail.com>
2025-05-29 17:58:38 +0700

    feat: init project

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
